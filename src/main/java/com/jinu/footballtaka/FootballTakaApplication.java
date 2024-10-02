34e79a89cc0fa21a6e604ddb6ea27b30754444c9
jinusong14@gmail.com <jinusong14@gmail.com>
2024-10-02 21:26:26 +0900

    [송진우] - 최초 프로젝트 생성

---
+++ src/main/java/com/jinu/footballtaka/FootballTakaApplication.java
@@ -0,0 +1,13 @@
+package com.jinu.footballtaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinu/footballtaka/FootballTakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinu.footballtaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
